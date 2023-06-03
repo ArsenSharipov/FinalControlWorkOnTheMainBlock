3f20395b649445a17774dc165d0c48aa2c399d9b
ArsenSharipov <arsen.sharipov.92@list.ru>
2023-06-04 03:48:10 +0500

    made the code

--- Program.cs
+++ Program.cs
@@ -1,1 +1,17 @@
-﻿
+﻿using System;
+using static System.Console;
+Clear();
+WriteLine($"Введите что угодно:");
+string[] input = ReadLine().Split();
+string[] output = new string[input.Length];
+int j = 0;
+for (int i = 0; i < input.Length; i++){
+    if(input[i].Length <= 3){
+        output[j] = input[i];
+        j++;
+    }
+}
+WriteLine($"Результат:");
+for (int k = 0; k < j; k++){
+    Write($"{output[k]} ");
+    }
